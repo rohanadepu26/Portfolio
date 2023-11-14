f6db7a4c381821376afc5948db80fdf74922f15e
Rohan Adepu <133311759+rohanadepu26@users.noreply.github.com>
2023-11-14 20:41:29 +0530

    Add files via upload
    Added files

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example JavaScript code
+document.getElementById('my-button').addEventListener('click', function() {
+    alert('Exiting portfolio page!');
+});
+
+// You can add more JavaScript functionality as needed
