ca631ed70ec4d48e97c9dc9ebd21044848798354
Rohan Adepu <rohanadepu26@gmail.com>
2024-03-06 20:02:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function exit() {
     alert('Exiting portfolio page!');
     alert('Thank you for visiting!');
 }
+document.querySelector('button').addEventListener('click', function() {
+    alert('Exiting portfolio page!');
+    alert('Thank you for visiting!');
+});
